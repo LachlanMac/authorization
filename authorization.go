b81114baf4f487032bddb99c82ae4bfd99332fb4
Lachlan Mccallum <lachlanmccallum@Lachlans-MacBook-Pro.local>
2018-12-28 11:08:04 -0500

    update 2

--- authorization.go
+++ authorization.go
@@ -1,8 +1,91 @@
 package authorization
 
+
+import (
+	"golang.org/x/crypto/bcrypt"
+	"database/sql"
+)
+
 type User struct {
 	
 	Username string
 	Password string
 	Email string
 }
+
+
+
+func GenerateHashSalt(password string) string{
+
+
+	passwordSlice := []byte(password)
+
+
+	hash, err := bcrypt.GenerateFromPassword(passwordSlice, bcrypt.MinCost)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+
+	return string(hash)
+
+}
+
+func VerifyHashedPassword(hashedPassword string, plainTextPassword []byte) bool{
+
+
+	byteHash := []byte(hashedPassword)
+
+	err := bcrypt.CompareHashAndPassword(byteHash, plainTextPassword)
+
+	if err == nil{
+		return true
+	}else{
+		return false
+	}
+
+}
+
+
+
+func UserExists(user User, db *sql.DB) bool{
+
+	var account_id int
+	sqlStatement := `SELECT account_id FROM users WHERE user_name=$1`
+	row := db.QueryRow(sqlStatement, user.Username)
+
+	err := row.Scan(&account_id)
+
+	if err == sql.ErrNoRows {
+		fmt.Println("USER NOT FOUND")
+		return false
+	}else if err != nil {
+		fmt.Println("UNCAUGHT ERROR : ", err)
+		return true
+	}else{
+		fmt.Println("USER ALREADY IN DB")
+		return true
+	}
+}
+
+func EmailExists(user User, db *sql.DB) bool{
+
+	var account_id int
+	sqlStatement := `SELECT account_id FROM users WHERE email=$1`
+	row := db.QueryRow(sqlStatement, user.Email)
+
+	err := row.Scan(&account_id)
+
+	if err == sql.ErrNoRows {
+		fmt.Println("USER NOT FOUND")
+		return false
+	}else if err != nil {
+		fmt.Println("UNCAUGHT ERROR : ", err)
+		return true
+	}else{
+		fmt.Println("USER ALREADY IN DB")
+		return true
+	}
+
+}
+
